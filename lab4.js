06f284b09cf808efbe22b61a4798eaa7b112bfa2
Alan Razo <a01703350@itesm.mx>
2022-02-22 09:58:00 -0600

    2

--- lab4.js
+++ lab4.js
@@ -1,9 +1,9 @@
 //1. Entrada: un número pedido con un prompt. Salida: Una tabla con los números del 1 al número dado con sus cuadrados y cubos. Utiliza document.write para producir la salida
 const numero = prompt("Ingresa un numero");
 console.log(numero);
-for (let i=1; i<=numero; i++){
-    var cuadrado=i*i;
-    var cubo=cuadrado*i;
+for (let i = 1; i <= numero; i++){
+    var cuadrado = i*i;
+    var cubo = cuadrado*i;
     document.write("<table><tr><td>",i,", ",cuadrado,", ",cubo,"</td></tr></table>");
     
 }
@@ -14,7 +14,7 @@ let value2 = Math.floor(Math.random() * 10) + 1;
 const res = prompt("Cuanto es "+ value1 +" + "+ value2 +"");
 console.log(res);
 
-if (value1+value2 == res){
+if (value1 + value2 == res){
     const respuesta = alert("Es correcto");
     console.log(respuesta); 
 }
@@ -24,32 +24,55 @@ else{
 }
 
 //3 .Función: contador. Parámetros: Un arreglo de números. Regresa: La cantidad de números negativos en el arreglo, la cantidad de 0's, y la cantidad de valores mayores a 0 en el arreglo. 
-
-
 function contador(arreglo){ 
-    for (let i=0; i<arreglo.lenght; i++){
-        if (i>0){
-            count1+=1;
+    let positivo = 0; 
+    let negativo = 0; 
+    let ceros = 0;
+
+    for (let i = 0; i < arreglo.length; i++) {
+        if (arreglo[i] > 0) {
+            positivo += 1;
         }
-        else if (i<0){
-            count2+=1;  
+        else if (arreglo[i] < 0) {
+            negativo += 1;  
         }
         else {
-            count3+=1;
+            ceros += 1;
         }
     }
+    alert("Negativos: " + negativo + " Ceros: " + ceros + " Positivos: " + positivo);
 }
-var count1=0, count2=0, count3=0;
 
 const arr = [-2,-58,0,0,0,0,1,2,3,5,6,7,8];
 contador(arr);
-var fun=contador;
-const mensaje = alert("Negativos: "+count2+" Ceros: "+count3+" Positivos: "+count1);
-console.log(mensaje);
 
 
+//4. Función: promedios. Parámetros: Un arreglo de arreglos de números. Regresa: Un arreglo con los promedios de cada uno de los renglones de la matriz.
+function promedios (numArray){
+    var average = [];
+    for (let i = 0; i < numArray.length; i++) {
+        average.push(numArray[i].reduce(function (x, y) { return x + y; }, 0) / numArray[i].length);
+    }
+    return average;
+}
 
+var items = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+  ];
+let answer = promedios(items);
+document.write("El promedio de cada renglon es: "+ answer);
 
+//5. Función: inverso. Parámetros: Un número. Regresa: El número con sus dígitos en orden inverso.
+function inverso (n){
+    n = n + "";
+	return n.split("").reverse().join("");
+}
+let num = 2022;
+document.write(" El numero inverso es: "+ inverso(num));
+
+//6. Ejercicio propio
 
 
 
