b3877944463a544ea6214738d9f078e19e24fa45
Alan Razo <a01703350@itesm.mx>
2022-04-21 23:12:34 -0500

    updated user model

--- Lab12,13,14/models/user.js
+++ Lab12,13,14/models/user.js
@@ -1,13 +1,11 @@
 const db = require('../util/database');
 const bcrypt = require('bcryptjs');
 
-const usuarios = [];
-
 module.exports = class User {
 
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(nuevo_usario, nuevo_passwd, nuevo_nombre) {
-        this.usuario = nuevo_usario;
+    constructor(nuevo_usuario, nuevo_passwd, nuevo_nombre) {
+        this.username = nuevo_usuario;
         this.password = nuevo_passwd;
         this.nombre = nuevo_nombre;
     }
@@ -15,24 +13,22 @@ module.exports = class User {
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
         return bcrypt.hash(this.password, 12)
-            .then(password_cifrado => {
-                return db.execute('INSERT INTO usuarios (npmbre, passwaor, username) VALUES (?,?,?)',
-                [this.usuario, password_cifrado, this.nombre]);
-
-            })
-            .catch(err => {
-                console.error(err);
-            });
+            .then( (password_cifrado) => {
+                return db.execute(
+                    'INSERT INTO usuarios (username, password, nombre) VALUES (?,?,?)', 
+                    [this.username, password_cifrado, this.nombre]);
+            }).catch((err) => {
+                console.log(err);
+            }); 
     }
 
-    //Este método servirá para devolver los objetos del almacenamiento persistente.
-    static login(usuario, password) {
-        
-        return true;
+    static fetchAll() {
+        return db.execute('SELECT * FROM usuarios');
     }
 
-    static findOne(usuario){
-        return db.execuete('SELECT * FROM usuario')
+    static findOne(username) {
+        return db.execute(
+            'SELECT * FROM usuarios WHERE username=?', [username]);
     }
 
 }
