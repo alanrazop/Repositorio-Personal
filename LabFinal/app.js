eee43613829cf6fcd4e9316c9eb3c015260864c0
Alan Razo <a01703350@itesm.mx>
2022-04-30 10:37:36 -0500

    Html dinamico incluido

--- LabFinal/app.js
+++ LabFinal/app.js
@@ -2,16 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const rutas_imperio = require('./routes/imperio.routes');
-
+const path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+//Varianle de configración de express (ejs)
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//Accede a una carpeta de archivos estáticos (css)
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Accede al directorio de rutas (express)
 app.use('/imperio', rutas_imperio);
 
+//Ruta nueva
 app.use('/desconocido', (request, response, next) => {
     response.send('Has entrado a una ruta desconocida :0'); 
 });
 
+//Ruta nueva
 app.use('/adios', (request, response, next) => {
     console.log(request.body);
     response.send('¡Nos vemos! Esto es una despediada :('); 
@@ -24,8 +34,8 @@ app.use((request, response, next) => {
 });
 
 app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    response.send('¡Bienvenidos al portal de Alan!'); //Manda la respuesta
+    console.error('Otro middleware!');
+    response.send('404 La página que buscas no existe'); //Manda la respuesta
 });
 
 app.listen(3000);
--- LabFinal/routes/imperio.routes.js
+++ LabFinal/routes/imperio.routes.js
@@ -6,4 +6,10 @@ router.get('/rutados', (request, response, next) => {
     response.send('Respuesta de la ruta "/imperio/rutados"'); 
 });
 
+//Devuelve un archivo HTML
+router.get('/hello', (request, response, next) => {
+    response.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
+
+
 module.exports = router;
